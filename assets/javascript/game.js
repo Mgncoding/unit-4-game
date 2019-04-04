f64c252153c4c61bd92ace7846ca94a746d1e4eb
unknown <naultmichael1@gmail.com>
2019-04-04 18:03:27 -0400

    new

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+
+// var for starting damage
+var heroOne = 6  //doubles
+var heroTwo = 5    
+var heroThree = 20
+var heroFour = 25
+// var for Max hp
+var HpOne = 120
+var HpTwo = 100
+var HpThree = 150
+var HpFour = 180
+
+var heroOneDph = $("#hero1")
+var heroTwoDph = $("#hero2")
+var heroThreeDph = $("#hero3")
+var heroFourDph = $("#hero4")
+var attackPosition = $("#attack-spot") 
+var enemyPosition = $("#enemy-spot")
+
+
+$("#hero1").on("click", function() {
+    attackposition.add$()
+    $("#hero1").append("#attack-spot")
+    
+
+    
+
+})
+
+
+
+    
